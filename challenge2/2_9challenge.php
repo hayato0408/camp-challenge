c32b6f8f4471c75d01f640fa04fdde913e521f69
hayato0408 <yasui0408hayato@gmail.com>
2016-06-14 11:28:25 +0900

    Add files via upload

---
+++ challenge2/2_9challenge.php
@@ -0,0 +1,22 @@
+
+
+９．以下の順で、連想配列を作成してください。
+　　　　1に'AAA', 'hello'に'world', 'soeda'に33, 20に20
+
+
+<?php
+
+
+
+$arr=array();
+$arr=array(
+          1=>'AAA',
+          'hello'=>'world',
+          'soeda'=>33,
+          20=>20,
+);
+
+echo $arr['hello'];
+
+
+?>
