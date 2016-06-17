45fca4c6211ce204e07308a1455f4a8414dda17b
hayato0408 <yasui0408hayato@gmail.com>
2016-06-17 15:27:52 +0900

    5_5challenge

---
+++ challenge5/5_5challenge.php
@@ -0,0 +1,12 @@
+５．ファイルアップロード機能を作成してください。
+
+
+<HTML>
+    <head>
+    </head>
+    <body>
+        <form enctype="multipart/form-data" action="sample.php" method="post">
+            ファイル選択：<input name="userfile" type="file" />
+        </form>
+    </body>
+</HTML>
