652c1862b23a3d4a5ebb1aea83c7c260ee4de8e2
hayato0408 <yasui0408hayato@gmail.com>
2016-06-17 13:03:19 +0900

    5_1challenge

---
+++ challenge5/5_1challenge.php
@@ -0,0 +1,33 @@
+１．以下の入力フィールドを持ったHTMLを、PHPで処理する想定で記述してください。
+　　　・名前（テキストボックス）、性別（ラジオボタン）、趣味（複数行テキストボックス）
+
+
+
+
+
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+  <title></title>
+
+</head>
+
+
+<body>
+
+<form action= "./sample.php" method="post">
+
+<imput type = "text"name = "txtName">
+
+<imput type = "radio"name = "rdoName">
+
+<text area name = "mulText">
+
+</textarea>
+
+</body>
+
+</html>
