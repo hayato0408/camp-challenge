438de096ed7df288bb453889c34b852ed6c91525
hayato0408 <yasui0408hayato@gmail.com>
2016-06-20 18:22:47 +0900

    challenge6_1

---
+++ challenge6/6_1challenge.php
@@ -0,0 +1,15 @@
+INSERT INTO profiles(profilesID,name,tell,age,birt' at line 1
+mysql> INSERT INTO profiles(profilesID,name,tell,age,birthday) VALUES(1,'田中実','012-345-6789',30,1994-02-01);
+Query OK, 1 row affected, 1 warning (0.00 sec)
+
+mysql> INSERT INTO profiles(profilesID,name,tell,age,birthday) VALUES(2,'鈴木茂','090-1122-3344',37,1987-08-12);
+Query OK, 1 row affected, 1 warning (0.00 sec)
+
+mysql> INSERT INTO profiles(profilesID,name,tell,age,birthday) VALUES(3,'鈴木実','080-5566-7788',24,2000-08-01);
+Query OK, 1 row affected, 1 warning (0.00 sec)
+
+mysql> INSERT INTO profiles(profilesID,name,tell,age,birthday) VALUES(4,'佐藤清','012-0987-6543',19,2005-08-01);
+Query OK, 1 row affected, 1 warning (0.00 sec)
+
+mysql> INSERT INTO profiles(profilesID,name,tell,age,birthday) VALUES(5,'高橋清','090-9900-1243',24,2000-12-24);
+Query OK, 1 row affected, 1 warning (0.00 sec)
