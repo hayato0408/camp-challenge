dacf825f5d79ee4813ca536d477d8e3cd3d800c8
hayato0408 <yasui0408hayato@gmail.com>
2016-06-22 15:30:32 +0900

    7_3challenge

---
+++ challenge7/7_3challenge.php
@@ -0,0 +1,19 @@
+課題3:前回の課題1で作成したテーブルからSELECT*により全件取得し、画面に取得した全情報を表示してください
+
+<?php
+
+try{
+  $pdo = new PDO('mysql:host = localhost;dbname=Challenge_db;charset=utf8','hayato','hone0044');
+}catch(PDOException $Exception){
+  die('接続に失敗しました:' .$Exception-> getMessage());
+}
+
+
+  $sql = "select * from profiles";
+  $query = $pdo -> prepare($sql);
+  $query -> execute();
+  $result = $query -> fetchall(PDO::FETCH_ASSOC);
+  var_dump($result);
+  $pdo = null;
+
+?>
