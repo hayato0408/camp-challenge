c776d4279bd527fca4e16c2509e97d6fe11bd789
hayato0408 <yasui0408hayato@gmail.com>
2016-06-27 10:39:32 +0900

    8_3challenge

---
+++ challenge8/8_3challenge.php
@@ -0,0 +1,28 @@
+3．以下の機能を持つクラスを作成してください。
+
+・パブリックな２つの変数
+・２つの変数に値を設定するパブリックな関数
+・２つの変数の中身をechoするパブリックな関数
+
+
+<?php
+  class menu{
+    public $food;
+    public $drink;
+    public function write($fo,$dr){
+            $this -> food = $fo;
+            $this -> drink  = $dr;
+    }
+    public function menu_list(){
+            return [$this -> food, $this -> drink];
+    }
+  }
+$sushi = new menu();
+
+
+$sushi -> write('大トロ', '河童巻き');
+$menu = $sushi -> menu_list();
+foreach ($menu as $key => $value) {
+  echo $value.'<br>';
+}
+?>
