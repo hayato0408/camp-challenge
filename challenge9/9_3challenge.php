40219c6c5748a4b85a094760969d87e10c2ff49c
hayato0408 <yasui0408hayato@gmail.com>
2016-06-27 16:41:02 +0900

    9_3challenge

---
+++ challenge9/9_3challenge.php
@@ -0,0 +1,13 @@
+３
+．baseという抽象クラスを作成し、以下を実装してください。
+・loadというprotectedな関数を用意してください。
+・showという公開関数を用意してください。
+
+<?php
+  abstract class base{
+    protected function load(){
+    }
+    public function show(){
+    }
+  }
+?>
