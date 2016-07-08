c2ed0955f5a5bf7e8ed394c2bea3801e35739e89
hayato0408 <yasui0408hayato@gmail.com>
2016-07-09 00:03:18 +0900

    no message

---
+++ 巨大なソースの修正　開発タスク　第２段階/UserManagementSystem-ver2.0/common/defineUtil.php
@@ -0,0 +1,15 @@
+<?php
+const ROOT_URL = 'index.php';     //indexディレクトリへのURL
+const TOP_URI = '/';                                //トップページ
+const INSERT = 'insert.php';                        //登録ページ
+const INSERT_CONFIRM = 'insert_confirm.php';        //登録確認ページ
+const INSERT_RESULT = 'insert_result.php';          //登録完了ページ
+const SEARCH = 'search.php';                         //検索ページ
+const SEARCH_RESULT = 'search_result.php';          //検索結果ページ
+
+//修正箇所　コメントを修正
+const RESULT_DETAIL = 'result_detail.php';          //詳細情報ページ
+const DELETE = 'delete.php';                        //削除確認ページ
+const DELETE_RESULT = 'delete_result.php';          //削除結果ページ
+const UPDATE = 'update.php';                         //更新ページ
+const UPDATE_RESULT = 'update_result.php';          //更新結果ページ
